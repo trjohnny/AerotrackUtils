63193cb0864dddf51fadbd448b6fe24cc1b431e3
Giovanni <jacket.gav@gmail.com>
2023-12-09 23:58:40 +0100

    fix: add headers, catch specific exceptions

--- src/main/java/com/aerotrack/utils/clients/ryanair/RyanairClient.java
+++ src/main/java/com/aerotrack/utils/clients/ryanair/RyanairClient.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import retrofit2.HttpException;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Query;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -21,6 +24,7 @@ import java.util.List;
 public class RyanairClient {
 
     private final RyanairApiService ryanairApiService;
+    private final static String RYANAIR_COOKIE = "rid.sig=jyna6R42wntYgoTpqvxHMK7H+KyM6xLed+9I3KsvYZaVt7P36AL6zp9dGFPu5uVxaIiFpNXrszr+LfNCdY3IT3oCSYLeNv/ujtjsDqOzkY66AL3V6kH2vsK+au12X21HkZ4S8GaG8CoBmm/m0rLsOKYkxtw+U3+ejBaPc15jJjKXnc3owMBg82SNbqyKjVd6Z6qcsoE25p3RmlcaHuHC3GBf1yIGtlqeQun3Mj0vmSURVPQBjK65pge2zGBymoORV6PsmZ0Nabv73gS2VkhG+Eccz5iSIRPrZm5cloi/941TFo8oiXdyqvTMx/ozox2fkvaKB2vd/goSx543TxPdKoGKRLaDY3FoIepe6I46UFvXEZYszzugXHYRnp0lbIn/HPyvHH/iW/TXRrqsELQabKd1hH+Ut1ZgpfsKEtwDVyL7mVvi1qEOqHddSVKCN/439KxQqi9K03dQDm+knQaLRpzZL8EYqCeSaosMOeEhc2CAYWLW2D5jH5iTot0YiyaN3QJFE59H3MYDpGjoZfTfen83yVSpT8DBahOOr6Eibv62bKQXBxel5kIm75dZB26iUzmkBs1Iags291UJ8wpu/GtBD1rghlZoRJt9u3ASkAj3P85dBcV8MwGykVWJ4mCO; mkt=/gb/en/; .AspNetCore.Session=CfDJ8NJy2CjeBXdEiTlIkEo9jP1x6eP6igWNkoeL9uCto1qdz97HQLRlCAJWIhw97YY5uemEBnTrcLNnoB7lqKOnEJRzF%2F4yhGKN9A5COUteaQmZduO6o2whfYqdyD1Qd%2B%2BH6EXjFP4cd0DtvPDwguugs%2Bc684C2CSfw16lyvYFtSkvU; fr-correlation-id=b8d2a2fe-383b-44fa-8c4a-ba6cab8c994d; rid=034ed121-51ec-4cac-9196-97a50ee42d2c; RY_COOKIE_CONSENT=true; STORAGE_PREFERENCES={\"STRICTLY_NECESSARY\":true,\"PERFORMANCE\":false,\"FUNCTIONAL\":false,\"TARGETING\":false,\"SOCIAL_MEDIA\":false,\"PIXEL\":false,\"GANALYTICS\":true,\"__VERSION\":2}; myRyanairID=";
 
     public static RyanairClient create() {
         Retrofit retrofit = new Retrofit.Builder()
@@ -33,12 +37,33 @@ public class RyanairClient {
 
     public List<Flight> getFlights(String fromAirportCode, String toAirportCode, LocalDate date) {
         try {
-            String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            String response = ryanairApiService.getFlights("1", fromAirportCode, toAirportCode, "false", "0", formattedDate, "false", "AGREED")
-                    .execute()
-                    .body();
-            return parseFlights(response, fromAirportCode, toAirportCode); // Parse the JSON string
-        } catch (Exception e) {
+
+            Response<String> response = ryanairApiService.getFlights(
+                    "rid.sig=jyna6R42wntYgoTpqvxHMK7H+KyM6xLed+9I3KsvYZaVt7P36AL6zp9dGFPu5uVxaIiFpNXrszr+LfNCdY3IT3oCSYLeNv/ujtjsDqOzkY66AL3V6kH2vsK+au12X21HkZ4S8GaG8CoBmm/m0rLsOKYkxtw+U3+ejBaPc15jJjKXnc3owMBg82SNbqyKjVd6Z6qcsoE25p3RmlcaHuHC3GBf1yIGtlqeQun3Mj0vmSURVPQBjK65pge2zGBymoORV6PsmZ0Nabv73gS2VkhG+Eccz5iSIRPrZm5cloi/941TFo8oiXdyqvTMx/ozox2fkvaKB2vd/goSx543TxPdKoGKRLaDY3FoIepe6I46UFvXEZYszzugXHYRnp0lbIn/HPyvHH/iW/TXRrqsELQabKd1hH+Ut1ZgpfsKEtwDVyL7mVvi1qEOqHddSVKCN/439KxQqi9K03dQDm+knQaLRpzZL8EYqCeSaosMOeEhc2CAYWLW2D5jH5iTot0YiyaN3QJFE59H3MYDpGjoZfTfen83yVSpT8DBahOOr6Eibv62bKQXBxel5kIm75dZB26iUzmkBs1Iags291UJ8wpu/GtBD1rghlZoRJt9u3ASkAj3P85dBcV8MwGykVWJ4mCO; mkt=/gb/en/; .AspNetCore.Session=CfDJ8NJy2CjeBXdEiTlIkEo9jP1x6eP6igWNkoeL9uCto1qdz97HQLRlCAJWIhw97YY5uemEBnTrcLNnoB7lqKOnEJRzF%2F4yhGKN9A5COUteaQmZduO6o2whfYqdyD1Qd%2B%2BH6EXjFP4cd0DtvPDwguugs%2Bc684C2CSfw16lyvYFtSkvU; fr-correlation-id=b8d2a2fe-383b-44fa-8c4a-ba6cab8c994d; rid=034ed121-51ec-4cac-9196-97a50ee42d2c; RY_COOKIE_CONSENT=true; STORAGE_PREFERENCES={\"STRICTLY_NECESSARY\":true,\"PERFORMANCE\":false,\"FUNCTIONAL\":false,\"TARGETING\":false,\"SOCIAL_MEDIA\":false,\"PIXEL\":false,\"GANALYTICS\":true,\"__VERSION\":2}; myRyanairID=",
+                    "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36",
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                    "en-US,en;q=0.5", // HEADERS
+
+                    "1",
+                    fromAirportCode, // INPUTS
+                    toAirportCode,
+                    "false",
+                    date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    "FALSE",
+                    "AGREED")
+                    .execute();
+
+            if (!response.isSuccessful()) {
+                throw new HttpException(response);
+            }
+
+            String responseBody = response.body();
+            if (responseBody == null) {
+                throw new HttpException(response);
+            }
+
+            return parseFlights(responseBody, fromAirportCode, toAirportCode); // Parse the JSON string
+        } catch (IOException | RuntimeException e) {
             log.error("Error in calling Ryanair API: " + e.getMessage());
             throw new RuntimeException("Caught exception while calling Ryanair API.", e);
         }
@@ -87,11 +112,15 @@ public class RyanairClient {
     public interface RyanairApiService {
         @GET("availability")
         retrofit2.Call<String> getFlights(
+                @Header("Cookie") String cookie,
+                @Header("User-Agent") String userAgent,
+                @Header("Accept") String accept,
+                @Header("Accept-Language") String acceptLanguage,
+
                 @Query("ADT") String adultCount,
                 @Query("Origin") String fromAirportCode,
                 @Query("Destination") String toAirportCode,
                 @Query("IncludeConnectingFlights") String includeConnectingFlights,
-                @Query("Disc") String discount,
                 @Query("DateOut") String dateOut,
                 @Query("RoundTrip") String roundTrip,
                 @Query("ToUs") String termsOfUse
