fb7753a55eadb9e9ef1745f8cef35009c325da6e
Giovanni <jacket.gav@gmail.com>
2024-03-10 16:39:30 +0100

    feat: airports objects constants

--- src/main/java/com/aerotrack/common/Constants.java
+++ src/main/java/com/aerotrack/common/Constants.java
@@ -3,4 +3,6 @@ package com.aerotrack.common;
 public class Constants {
     public static final String FLIGHT_TABLE_ENV_VAR = "FLIGHT_TABLE";
     public static final String AIRPORTS_BUCKET_ENV_VAR = "AIRPORTS_BUCKET";
+    public static final String RYANAIR_AIRPORTS_OBJECT_NAME = "ryanair_airports.json";
+    public static final String WIZZAIR_AIRPORTS_OBJECT_NAME = "wizzair_airports.json"
 }
